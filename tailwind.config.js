10537c60234fe6ab9bebc16e36e8bf3c72a675c2
Walisson Gomes <walissonwaal@gmail.com>
2023-03-20 17:01:39 -0300

    Configurações

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: '#00AC00',
+        red: '#B00000',
+        yellow: '#E6BF00',
+        gray: '#BABABA',
+        background: '#EEEEEE',
+        white: '#FFFFFF'
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      },
+      container: {
+        center: true,
+      }
+    },
+  },
+  plugins: [],
+}
