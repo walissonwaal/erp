a785f6759227928cf6b6521ce12b2a494686fbe4
Walisson Gomes da Silva <walissonwaal@gmail.com>
2023-06-17 12:02:25 -0300

    Ajuste da rota Login.

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Login from '@/src/Layout/Login'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function login() {
-  return (
-    <div className='home'>
-      <Login />
-    </div>
-  )
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkToken = () => {
+      if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+          // O usuário está logado, redireciona para a página de dashboard
+          router.push('/dashboard');
+        }else {
+          router.push('/login');
+        }
+      }
+    };
+
+    checkToken();
+  }, [router]);
+  return null
 }
